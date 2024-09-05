c5921ca7aeec76bf8c2a38201568fa61c7849893
lgm5220 <lgm5220@psu.edu>
2024-09-05 18:00:04 -0400

    Update
    Current delta is 236.4

--- project_1/src/features/clean_reformat.R
+++ project_1/src/features/clean_reformat.R
@@ -4,7 +4,6 @@ house_dt <- fread("./volume/data/raw/Stat_380_housedata.csv")
 qc_data <- fread("./volume/data/raw/Stat_380_QC_table.csv")
 ex_sub <- fread("./volume/data/raw/example_sub.csv")
 
-# Create train and test split
 # average of houses that share the same value, local averages
 train <- house_dt[grep("train_", house_dt$Id)]
   
@@ -22,9 +21,11 @@ ordered_test <- test[order(sort_col)]
 # after rows are in order, group the train by something, get average saleprice in group
 # Note: train has values. Test does not.
 # Create new column, average salesprice column
-avg_price_by_group <- 
-  
+avg_price_by_group <- train[, .(mean_prices = mean(SalePrice)), by = qc_code]
+
 # merge the avg table to the test table, overwrite salesprice house_dt col with prev results.
+ordered_test[avg_price_by_group, on = "qc_code", SalePrice := mean_prices]
+global_avg <- mean(ordered_test$SalePrice, na.rm = TRUE)
 
 # select columns Id and SalePrice from test
 submit <- test[,.(Id, SalePrice)]
