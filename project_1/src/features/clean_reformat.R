ae6fd06f2085c855391a7feb27e5bc3b121fb7c1
lgm5220 <lgm5220@psu.edu>
2024-09-10 13:49:58 -0400

    Clean Comments
    Since I need to submit, clean up the info

--- project_1/src/features/clean_reformat.R
+++ project_1/src/features/clean_reformat.R
@@ -4,31 +4,27 @@ house_dt <- fread("./volume/data/raw/Stat_380_housedata.csv")
 qc_data <- fread("./volume/data/raw/Stat_380_QC_table.csv")
 ex_sub <- fread("./volume/data/raw/example_sub.csv")
 
-# average of houses that share the same value, local averages
+# Create new column vector, house_dt, totaling the half and full baths
 house_dt[, total_bathrooms := HalfBath * 0.5 + FullBath]
 
 train <- house_dt[grep("train_", house_dt$Id)]
   
 test <- house_dt[grep("test_", house_dt$Id)]
 
-# g sub function (gsub(find and replace) and grepl)
-  #- find "test_" replace with "" put in new col (mutate)
-# create new columns that are numeric (test_xxx)
-# put rows in order (ascending order on test vals)
-# ordered_train <- train[order(Id)]
 test$sort_col <- gsub("test_", "", test$Id)
 test$sort_col<- as.integer(test$sort_col)
 ordered_test <- test[order(sort_col)]
   
-# after rows are in order, group the train by something, get average saleprice in group
-# Create new column, average salesprice column
+# find average price by qc_code and total_bathrooms
 avg_price_by_group <- train[, .(mean_prices = mean(SalePrice)), by = .(qc_code, total_bathrooms)]
 
 # merge the avg table to the test table, overwrite salesprice house_dt col with prev results.
 ordered_test[avg_price_by_group, on = .(qc_code, total_bathrooms), SalePrice := mean_prices]
 
+# take NA cases
 na_cases <- ordered_test[is.na(SalePrice)]
 
+# retrieve global_avg for any remaining NAs that were not sorted
 global_avg <- mean(ordered_test$SalePrice, na.rm = TRUE)
 
 if (nrow(ordered_test[is.na(SalePrice)]) > 0) {
